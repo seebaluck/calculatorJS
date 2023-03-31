02c08eda2a8ab5c80a8582aa2e03bd8b6c81c2f3
Satee S <satee.seebaluck@gmail.com>
2023-03-31 22:46:09 +0200

    handle error for substract method

--- substract.js
+++ substract.js
@@ -1,5 +1,8 @@
-module.exports = substract(a,b){
-    return a - b
-/* commentaire*/
-
-}
+module.exports = function substrac(a, b) {
+    const res = a - b
+    if(Number.isNaN(res)) {
+      throw new Error('Invalid Operation')
+    }
+   
+    return res
+  }
